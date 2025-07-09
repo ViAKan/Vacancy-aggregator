910795911bf6afa8a9cd7e94948bd0ed6cf6abf5
ARKADIY <danilov-av2004@mail.ru>
2025-07-09 12:10:09 +0300

    fixed reviewdog

--- app/src/main/java/ru/practicum/android/diploma/util/UiUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/UiUtils.kt
@@ -10,18 +10,20 @@ import java.util.Locale
 
 fun dpToPx(dp: Float, context: Context): Int {
     return TypedValue.applyDimension(
-        TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        context.resources.displayMetrics
     ).toInt()
 }
 
-//Может использоваться для ручной настройки локали
+// Может использоваться для ручной настройки локали
 fun getLocalizedContext(base: Context, locale: Locale): Context {
     val config = Configuration(base.resources.configuration)
     config.setLocale(locale)
     return base.createConfigurationContext(config)
 }
 
-//общая функция для скрытия клавиатуры
+// общая функция для скрытия клавиатуры
 fun EditText.hideKeyboardOnDone(context: Context) {
     setOnEditorActionListener { _, actionId, _ ->
         if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -30,6 +32,8 @@ fun EditText.hideKeyboardOnDone(context: Context) {
                 context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(windowToken, 0)
             true
-        } else false
+        } else {
+            false
+        }
     }
 }
