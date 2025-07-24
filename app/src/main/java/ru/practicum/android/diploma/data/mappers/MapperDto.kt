59d935d19894d9a4451b3d6203977e63eca7b34d
ARKADIY <danilov-av2004@mail.ru>
2025-07-24 21:22:04 +0300

    refactored review dog

--- app/src/main/java/ru/practicum/android/diploma/data/mappers/MapperDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/mappers/MapperDto.kt
@@ -17,7 +17,6 @@ import ru.practicum.android.diploma.domain.models.vacancies.Vacancy
 import ru.practicum.android.diploma.domain.models.vacancydetails.EmploymentForm
 import ru.practicum.android.diploma.domain.models.vacancydetails.EmploymentFormType
 import ru.practicum.android.diploma.domain.models.vacancydetails.VacancyDetails
-import ru.practicum.android.diploma.util.ResponseType
 
 fun VacanciesDto.toDomain(): Vacancy {
     return Vacancy(
--- app/src/main/java/ru/practicum/android/diploma/util/ResponseType.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ResponseType.kt
@@ -1,13 +1,30 @@
 package ru.practicum.android.diploma.util
 
+import ru.practicum.android.diploma.util.ResponseCode.CODE_ID_OTHER_REGIONS
+import ru.practicum.android.diploma.util.ResponseCode.CODE_NO_CONNECTION
+import ru.practicum.android.diploma.util.ResponseCode.CODE_REQUEST_FAILED
+import ru.practicum.android.diploma.util.ResponseCode.CODE_SEARCH_SUCCESS
+import ru.practicum.android.diploma.util.ResponseCode.CODE_SERVER_ERROR
+import ru.practicum.android.diploma.util.ResponseCode.CODE_UNKNOWN
+
 enum class ResponseType(val code: Int) {
-    NO_CONNECTION(-1),
-    SEARCH_SUCCESS(2),
-    SERVER_ERROR(5),
-    REQUEST_FAILED(1),
-    ID_OTHER_REGIONS(1001),
-    UNKNOWN(-999)
+    NO_CONNECTION(CODE_NO_CONNECTION),
+    SEARCH_SUCCESS(CODE_SEARCH_SUCCESS),
+    SERVER_ERROR(CODE_SERVER_ERROR),
+    REQUEST_FAILED(CODE_REQUEST_FAILED),
+    ID_OTHER_REGIONS(CODE_ID_OTHER_REGIONS),
+    UNKNOWN(CODE_UNKNOWN);
 }
 
 fun Int.toResponseType(): ResponseType =
     ResponseType.values().find { it.code == this } ?: ResponseType.UNKNOWN
+
+
+object ResponseCode {
+    const val CODE_NO_CONNECTION = -1
+    const val CODE_SEARCH_SUCCESS = 2
+    const val CODE_SERVER_ERROR = 5
+    const val CODE_REQUEST_FAILED = 1
+    const val CODE_ID_OTHER_REGIONS = 1001
+    const val CODE_UNKNOWN = -999
+}
